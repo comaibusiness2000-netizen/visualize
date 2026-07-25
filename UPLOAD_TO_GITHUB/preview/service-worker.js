7d49466df07ad00ed81924f72d8628dedc28550a
samuele <comaibusiness2000@gmail.com>
2026-07-25 15:00:57 +0200

    Fix life tab mobile layout

--- UPLOAD_TO_GITHUB/preview/service-worker.js
+++ UPLOAD_TO_GITHUB/preview/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "visualize-preview-v92";
-const APP_VERSION = "92";
+const CACHE_NAME = "visualize-preview-v93";
+const APP_VERSION = "93";
 const APP_SHELL = [
   "./privacy.html",
   "./support.html",
