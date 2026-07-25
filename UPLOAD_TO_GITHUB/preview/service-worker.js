35184aeed0fb0fbc00667f641ef05009911c2d11
samuele <comaibusiness2000@gmail.com>
2026-07-25 16:59:17 +0200

    Fix mobile viewport nav spacing

--- UPLOAD_TO_GITHUB/preview/service-worker.js
+++ UPLOAD_TO_GITHUB/preview/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "visualize-preview-v95";
-const APP_VERSION = "95";
+const CACHE_NAME = "visualize-preview-v96";
+const APP_VERSION = "96";
 const APP_SHELL = [
   "./privacy.html",
   "./support.html",
