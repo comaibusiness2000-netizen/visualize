6e5e0b57aca90294827fc5ef521b445e6be15b7b
samuele <comaibusiness2000@gmail.com>
2026-07-27 18:01:55 +0200

    Increase life runway contrast

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "visualize-preview-v129";
-const APP_VERSION = "129";
+const CACHE_NAME = "visualize-preview-v130";
+const APP_VERSION = "130";
 const APP_SHELL = [
   "./privacy.html",
   "./support.html",
