790786b26e29d8befb79658b8f24aa3f1b02625d
samuele <comaibusiness2000@gmail.com>
2026-07-27 15:00:38 +0200

    Refine profile drawer experience

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "visualize-preview-v123";
-const APP_VERSION = "123";
+const CACHE_NAME = "visualize-preview-v124";
+const APP_VERSION = "124";
 const APP_SHELL = [
   "./privacy.html",
   "./support.html",
