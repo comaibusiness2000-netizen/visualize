c71af2a7a70a95e45c500ca76c981e9207f0593c
samuele <comaibusiness2000@gmail.com>
2026-08-09 14:43:32 +0200

    Fix native file persistence import

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ import {
   Vibration,
   View
 } from "react-native";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as ImagePicker from "expo-image-picker";
 import * as Speech from "expo-speech";
 
